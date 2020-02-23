2d04999fca9d415ccc6c47a6ff9530283de7eca4
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-02-23 21:36:31 +0100

    create basic model for application

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,4 +1,55 @@
 const budgetController = (function () {
+  const Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
+
+  const addItem = function(type, description, value) {
+    let newItem, ID;
+
+    // Create new ID
+    if (data.allItems[type].length > 0) {
+      ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+    } else {
+      ID = 0;
+    }
+
+    // Create new item based on type
+    if (type === "inc") {
+      newItem = new Income(ID, description, value);
+    } else if (type === "exp") {
+      newItem = new Expense(ID, description, value);
+    } else {
+      console.log("Unknown item type (needs to be inc or exp)!");
+    }
+
+    // Add new item to list and return it
+    data.allItems[type].push(newItem);
+    return newItem;
+  };
+
+  return {
+    addItem: addItem,
+  };
+
 })();
 
 
@@ -28,11 +79,14 @@ const UIController = (function() {
 
 const appController = (function(budgetCtrl, UICtrl) {
   const addButtonClick = function() {
+    let input, newItem;
+
     // 1. Get field input data
-    const input = UICtrl.getFieldInput();
-    console.log(input);
+    input = UICtrl.getFieldInput();
 
     // 2. Add item to budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
     // 3. Add item to ui controller
     // 4. Calculate budget
     // 5. Update budget in UI
@@ -57,7 +111,7 @@ const appController = (function(budgetCtrl, UICtrl) {
   };
 
   return {
-    init: init
+    init: init,
   };
 
 })(budgetController, UIController);
