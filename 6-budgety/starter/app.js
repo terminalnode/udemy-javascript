b7bc2f8d20e65f1e3f5e622b4910939485b16958
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-02-23 21:04:56 +0100

    add methods for reading input data

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -3,8 +3,50 @@ const budgetController = (function () {
 
 
 const UIController = (function() {
+  const DOMClasses = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn",
+  }
+
+  const getFieldInput = function() {
+    return {
+      type: document.querySelector(DOMClasses.inputType).value, // will be inc or exp
+      description: document.querySelector(DOMClasses.inputDescription).value,
+      value: document.querySelector(DOMClasses.inputValue).value
+    };
+  }
+
+  return {
+    getFieldInput: getFieldInput,
+    DOMClasses: DOMClasses,
+  };
 })();
 
 
 const appController = (function(budgetCtrl, UICtrl) {
+  const DOMClasses = UICtrl.DOMClasses;
+
+  const addButtonClick = function() {
+    // 1. Get field input data
+    const input = UICtrl.getFieldInput();
+    console.log(input);
+
+    // 2. Add item to budget controller
+    // 3. Add item to ui controller
+    // 4. Calculate budget
+    // 5. Update budget in UI
+  }
+
+  const keypressEvent = function(event) {
+    // keyCode 13 is enter/return
+    if (event.keyCode === 13) {
+      addButtonClick();
+    }
+  }
+
+  document.querySelector(DOMClasses.inputButton).addEventListener("click", addButtonClick);
+  document.addEventListener("keypress", keypressEvent);
+
 })(budgetController, UIController);
